aa18b2f5febddd4ab986115e3bf24bc40035d6be
Austin Oblow <austinoblow97@gmail.com>
2023-07-04 13:42:56 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const names = ["Charlie", "Samip", "Ali"]
+
+let messages = []
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+
+    return messages;
+}
+
+
+
+let num = 10;
+function countDown(num) {
+    while (num >= 0)
+        console.log(num--);
+}
+
+console.log(num);
