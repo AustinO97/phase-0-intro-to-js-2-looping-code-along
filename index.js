06dd68879ac5b9cc8efa6f7c5c62ee7a4fdcc828
Austin Oblow <austinoblow97@gmail.com>
2023-07-25 16:45:19 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
 
-const names = ["Charlie", "Samip", "Ali"]
+const gifts = ["teddy bear", "drone", "doll"];
 
-let messages = []
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+const names = ['Charlie', 'Samip', 'Ali'];
+const messages = [];
 
 function writeCards(names) {
     for (let i = 0; i < names.length; i++) {
-        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
     }
-
     return messages;
 }
 
+const number = 10;
 
-
-let num = 10;
-function countDown(num) {
-    while (num >= 0)
-        console.log(num--);
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
 }
-
-console.log(num);
